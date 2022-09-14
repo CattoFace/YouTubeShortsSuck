171d628be8025a9ed4524d2873344faaa83323c3
CattoFace <barr520@gmail.com>
2022-09-14 08:31:18 +0300

    reduced perms and bugfix

--- ShortsSuck/YTSS.js
+++ ShortsSuck/YTSS.js
@@ -17,9 +17,13 @@ function waitForElm(selector) {
         });
     });
 }
-waitForElm("div#actions").then((menu) =>{
-	btn = document.createElement("button")
-	btn.textContent = "Full Player"
-	btn.addEventListener("click", function(){location.href =  location.href.replace("shorts/","watch?v=")})
-	menu.appendChild(btn)
-})
+
+function addbutton(){
+	waitForElm("div#actions").then((menu) =>{
+		btn = document.createElement("button")
+		btn.textContent = "Full Player"
+		btn.addEventListener("click", function(){location.href =  location.href.replace("shorts/","watch?v=")})
+		menu.appendChild(btn)
+})}
+
+window.addEventListener('yt-page-data-updated',() => addbutton())
